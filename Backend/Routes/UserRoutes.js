a9c9fb0bb9b4ec2983519cc58b8db071fc4da61d
Charuni Fernando <charunifernandz@gmail.com>
2025-04-05 16:59:07 +0530

    update user routes
    update user routes

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../Controllers/UserControllers");
 
+// image uploading function for users
 router.get("/", UserController.getAllUser);
 router.post("/", UserController.upload.single('profilePhoto'), UserController.addUser); // Upload file when creating user (client)
 router.get("/:id", UserController.getById);
